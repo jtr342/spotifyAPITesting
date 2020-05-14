39a5aa20c8eea8fa0da4a5ab00d4a17d0984395b
Justin Rabe <jtr342@gmail.com>
2020-05-13 21:58:50 -0700

    React App Start

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 const fetch = require("node-fetch");
 const url = "https://api.spotify.com/v1/albums/{6Lq1lrCfkpxKa4jCo5gKWr}";
+var album;
 fetch(url)
     .then(response => response.json())
     .then(data => console.log(data));
